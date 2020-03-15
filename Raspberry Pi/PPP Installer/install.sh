7fd35400b85b396160bf9920f7a4b4ec2445d7c6
arturb-dev <darkocho@gmail.com>
2020-03-15 12:42:06 +0200

    Update install.sh

--- Raspberry Pi/PPP Installer/install.sh
+++ Raspberry Pi/PPP Installer/install.sh
@@ -26,17 +26,29 @@ if ! (grep -q 'route' /etc/ppp/ip-up ); then
     echo "sudo route del default" >> /etc/ppp/ip-up
     echo "sudo route add default ppp0" >> /etc/ppp/ip-up
 fi
-
-mv reconnect.sh /usr/src/
-mv nbiot.service /etc/systemd/system/
 			  
 systemctl daemon-reload
-# systemctl enable nbiot.service
-# sudo pon # to go online for NBIOT modem
-# sudo poff # to go offline for NBIOT modem
 
-echo "${YELLOW}To connect to NBIOT modem run ${BLUE}\"sudo pon\"${YELLOW} and to disconnect run ${BLUE}\"sudo poff\" ${SET}"
-echo "${YELLOW}To Run as Service ${BLUE}\"sudo systemctl enable nbiot.service\" ${SET}"
+echo "${YELLOW}Do you want to activate auto connect/reconnect service at R.Pi boot up? [Y/n] ${SET}"
+read auto_reconnect
+while [ 1 ]
+do
+	case $auto_reconnect in
+		[Yy]* )    echo "${YELLOW}Downloading setup file${SET}"
+		
+			mv reconnect.sh /usr/src/
+			mv nbiot.service /etc/systemd/system/
+			  
+			systemctl daemon-reload
+			systemctl enable reconnect.service
+			  
+			break;;
+			  
+		[Nn]* )    echo "${YELLOW}To connect to internet run ${BLUE}\"sudo pon\"${YELLOW} and to disconnect run ${BLUE}\"sudo poff\" ${SET}"
+			  break;;
+		*)   echo "${RED}Wrong Selection, Select among Y or n${SET}";;
+	esac
+done
 
 read -p "Press ENTER key to reboot" ENTER
 reboot
